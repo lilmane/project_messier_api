b89079b13a86df3611ea1f74025ed3a538a5a6ea
Lounis <45457699+lilmane@users.noreply.github.com>
2020-02-29 22:47:39 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import requests
+import csv
+import sqlite3
+import pandas as pd
+
+
+
+# Database creation
+
+conn = sqlite3.connect('messier.db')
+c = conn.cursor()
+
+
+
+app = Flask(__name__)
+
+
+CSV_URL = 'https://www.datastro.eu/explore/dataset/catalogue-de-messier/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true&csv_separator=%3B'
+
+
+@app.route('/Messier')
+
+def messier():
+
+    df = pd.read_csv(CSV_URL, sep=';') 
+
+    df.to_html('messier_csv.html') 
+    html_table = df.to_html()
+
+
+    return html_table
+
+
+    if response.status_code != 200:
+        return "Status Error"
+
+       
+            
+if __name__ == "__main__":
+    app.run(debug=True)
